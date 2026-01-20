187162c543a602265322ddc30688647742cb4fb8
ArielSantos-V4 <ariel.santos@v4company.com>
2026-01-20 17:27:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ with aba_dashboard:
         client = gspread.authorize(creds)
     
         sheet = client.open_by_key("13EPwhiXgh8BkbhyrEy2aCy3cv1O8npxJ_hA-HmLZ-pY")
-        worksheet = sheet.get_worksheet(0)
+        worksheet = sheet.get_worksheet(6)
     
         data = worksheet.get_all_records()
         return pd.DataFrame(data)
