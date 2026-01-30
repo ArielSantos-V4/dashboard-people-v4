ea3c187a0302917e735789e6149f950e74910c30
ArielSantos-V4 <ariel.santos@v4company.com>
2026-01-30 13:11:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -252,6 +252,20 @@ st.set_page_config(
     page_icon="LOGO VERMELHO.png"
 )
 
+st.markdown("""
+<style>
+/* REMOVE COMPLETAMENTE O HEADER DO st.dialog */
+div[role="dialog"] > div > header {
+    display: none !important;
+}
+
+/* Remove o espaço que o header deixava */
+div[role="dialog"] > div {
+    padding-top: 0px !important;
+}
+</style>
+""", unsafe_allow_html=True)
+
 # ==============================
 # LOGIN
 # ==============================
